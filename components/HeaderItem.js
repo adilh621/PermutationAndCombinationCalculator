1bdbbbce2bbeeec3cf3d767e86b235c4a2ecfa56
Adil Hussain <adil@adils-mbp.myfiosgateway.com>
2023-01-16 06:57:19 -0500

    fixed about creator button

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -10,14 +10,14 @@ function HeaderItem({Icon , title}) {
 
     return (
 
-    <div className="flex flex-col items-center cursor-pointer group w-8 sm:w-20 hover:text-black">
+    <a href={links.website} className="flex flex-col items-center cursor-pointer group w-8 sm:w-20 hover:text-black">
         <Icon className='h-8 mb-1 group-hover:animate-bounce'/>
 
         
         <a href="https://adil-h.vercel.app/" className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</a>
         
         
-    </div>
+    </a>
     )
 }
 export default HeaderItem;
